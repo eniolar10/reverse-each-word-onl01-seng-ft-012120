598c1e2db17427c54ec3ab4e1893d0c7d31af882
Ola Enol <eniolar10@gmail.com>
2020-02-03 15:55:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,13 @@ array = reverse_each_word.split
 def reverse(array)
   array.each do |array|
 end
+end
 
+def rever(word)
+rever("Hello there, and how are you?")
+  word = rever.split
+  word.collect do |word|
+  end 
+end
 
 
